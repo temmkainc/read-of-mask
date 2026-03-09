83dbad39f9aa675924d990393b1efe2b35a07806
Artem <temmkainc@gmail.com>
2026-03-10 00:38:46 +0100

    add: Faceless Middle Finger Animation reflecting a player

--- Assets/_Project/Scripts/Interaction/Interactables/FacelessCharacter/FacelessCharacter.cs
+++ Assets/_Project/Scripts/Interaction/Interactables/FacelessCharacter/FacelessCharacter.cs
@@ -1,6 +1,32 @@
+using System;
 using UnityEngine;
+using Zenject;
 
 public class FacelessCharacter : LookCloserInteractableBase
 {
+    [SerializeField] private Animator _animator;
+    private InputManager _inputManager;
 
+    private readonly int DoShowMiddleFingerHash = Animator.StringToHash("DoShowMiddleFinger");
+    private readonly int DoHideMiddleFingerHash = Animator.StringToHash("DoHideMiddleFinger");
+
+    [Inject]
+    public void Construct(InputManager inputManager)
+    {
+        _inputManager = inputManager;
+        _inputManager.ShowMiddleFingerAction.performed += On_MiddleFingerPressed;
+        _inputManager.ShowMiddleFingerAction.canceled += On_MiddleFingerReleased;
+    }
+
+    private void On_MiddleFingerReleased(UnityEngine.InputSystem.InputAction.CallbackContext context)
+    {    
+        _animator.ResetTrigger(DoShowMiddleFingerHash);
+        _animator.SetTrigger(DoHideMiddleFingerHash);
+    }
+
+    private void On_MiddleFingerPressed(UnityEngine.InputSystem.InputAction.CallbackContext context)
+    {
+        _animator.ResetTrigger(DoHideMiddleFingerHash);
+        _animator.SetTrigger(DoShowMiddleFingerHash);
+    }
 }
